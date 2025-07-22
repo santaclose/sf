75734187579bc36f0f1ffb0b1ff580348878682e
Santiago <san@san>
2025-07-22 22:24:14 +0200

    add missing voxel volume vertex shader

---
+++ assets/shaders/voxelVolume.vert.glsl
@@ -0,0 +1,30 @@
+#version 460
+
+#include <assets/shaders/particle.h>
+
+layout(location = 4) out vec2 fTexCoords;
+
+layout(location = 0) in vec3 vPosition;
+layout(location = 5) in vec2 vTexCoords;
+
+layout(binding = 0) uniform SharedGpuData
+{
+	mat4 modelMatrix;
+	mat4 cameraMatrix;
+	float cameraPositionX;
+	float cameraPositionY;
+	float cameraPositionZ;
+	float windowSizeX;
+	float windowSizeY;
+};
+
+layout (std430, binding = 1) buffer PerParticleBuffer
+{
+	float perParticleData[];
+};
+
+void main()
+{
+	fTexCoords = vTexCoords;
+	gl_Position = cameraMatrix * vec4(PARTICLE_INITIAL_POSITION + vPosition * PARTICLE_INITIAL_SCALE, 1.0);
+}

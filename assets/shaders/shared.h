4179d646437db45317228904d914e0a6108eb77d
Santiago <san@san.com>
2025-08-26 13:56:37 -0400

    shared shader file fix

--- assets/shaders/shared.h
+++ assets/shaders/shared.h
@@ -1,4 +1,4 @@
 
-#define WINDOW_SIZE_VEC vec2(windowSizeX, windowSize.Y)
+#define WINDOW_SIZE_VEC vec2(windowSizeX, windowSizeY)
 #define CAMERA_POSITION_VEC vec3(cameraPositionX, cameraPositionY, cameraPositionZ)
 #define PIXEL_SPACE_TO_GL_SPACE(vecin) vec4(vecin.x / windowSizeX * 2.0 - 1.0, -vecin.y / windowSizeY * 2.0 + 1.0, 0.0, 1.0)

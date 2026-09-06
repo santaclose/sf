b143ce30ffda348c0a04da62b23e4561c599faa3
Santiago <san@san>
2026-09-06 14:47:12 +0200

    fix imvec operators

--- src/Renderer/GlImGuiDisplayPanel.cpp
+++ src/Renderer/GlImGuiDisplayPanel.cpp
@@ -5,15 +5,15 @@
 #include <iostream>
 #include <cstdint>
 
-bool operator!=(const ImVec2& a, const ImVec2& b)
+inline bool operator!=(const ImVec2& a, const ImVec2& b)
 {
 	return a.x != b.x || a.y != b.y;
 }
-ImVec2 operator+(const ImVec2& a, const ImVec2& b)
+inline ImVec2 operator+(const ImVec2& a, const ImVec2& b)
 {
 	return ImVec2(a.x + b.x, a.y + b.y);
 }
-ImVec2 operator-(const ImVec2& a, const ImVec2& b)
+inline ImVec2 operator-(const ImVec2& a, const ImVec2& b)
 {
 	return ImVec2(a.x - b.x, a.y - b.y);
 }
